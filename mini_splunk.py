a3b1114e763cdb8a0f2f2be9a2d6e037abe9d60a
Rene <devproren@gmail.com>
2014-04-23 15:22:49 -0700

    modified python solution to print to stdout by default if output file is not specified

--- mini_splunk.py
+++ mini_splunk.py
@@ -8,30 +8,32 @@
 
 """
 
+import sys
 import argparse
 
-def search(search_string, log_file, out_file):
+def search(search_string, log_file, out_file=None):
     f = open(log_file, 'r')
     lines = f.readlines()
     lines.reverse()
     f.close()
 
-    with open(out_file, 'w') as f:
+    if out_file == None:
         for line in lines:
             if search_string in line:
-                f.write(line)
+                print(line[:-1])
+    else:
+        with open(out_file, 'w') as f:
+            for line in lines:
+                if search_string in line:
+                    f.write(line)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Output lines that match given search string in reverse chronological order. Search string is case sensitive.')
     parser.add_argument('logfile', help='Name of log file. Should be in chronological order.')
     parser.add_argument('string', help='Input string to search for.')
-    parser.add_argument('-o', '--output', help='Use own specified output file name. Defaults to output.log')
+    parser.add_argument('-o', '--output', help='Use own specified output file name. Default to stdout.')
     args = parser.parse_args()
  
-    out_file = 'output.log'
-    if args.output:
-        out_file = args.output
-
-    search(args.string, args.logfile, out_file)
+    search(args.string, args.logfile, args.output)
 
